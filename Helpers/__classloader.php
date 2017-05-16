4c3b702f8e3bbb558a877a77af51998d04bfd170
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-05-16 15:08:15 +0100

    Added API files

---
+++ Helpers/__classloader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author
+ * Banjo Mofesola Paul
+ * Chief Developer, Planet NEST
+ * mofesolapaul@planetnest.org
+ * 16/05/2017 11:04
+ */
+
+spl_autoload_register(function($classname) {
+    $classname = preg_replace('/\\\/', DIRECTORY_SEPARATOR, $classname);
+    $classname = $classname . '.php';
+    $classname = __DIR__ . '/../Classes/' . $classname;
+
+    if (file_exists($classname)) require_once $classname;
+});
