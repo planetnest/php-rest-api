6b4efc8eb83e0eba262da2ae875a92cc6a0e2786
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-10-19 11:40:23 +0100

    Updated example Resource

--- Classes/Users.php
+++ Classes/Users.php
@@ -7,25 +7,27 @@
  * mofesolapaul@planetnest.org
  * 16/05/2017 12:02
  */
-class Users extends Apiable
+class Users
 {
 
-    /**
-     * @param null|\Api\API $api
-     * @return string
-     */
-    function all($api = null) {
-        if ($api->method !== 'GET') return "Users: Invalid invocation";
+    use \Traits\HttpMethodCheck;
+    use \Traits\DbTransaction;
+
+    function index() {
+        return 'Welcome to Users resource';
+    }
+
+    function all($request) {
+        $this->_checkMethod("GET", $request->method);
         return "All users";
     }
 
-    /**
-     * @param null|\Api\API $api
-     * @internal param $args
-     * @return string
-     */
-    function one($api = null) {
-        if ($api->method !== 'POST') return "Users: Invalid invocation";
+    function one($request) {
+        $this->_checkMethod("POST", $request->method);
         return "One user";
+    }
+
+    function db($request) {
+        
     }
 }
